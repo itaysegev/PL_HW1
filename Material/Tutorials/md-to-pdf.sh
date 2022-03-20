2fd65665d3341575847b60126f826fc8ea394dc0
Yair Twito <yairt16@gmail.com>
2022-03-20 18:30:21 +0200

    remove irrelevant files

--- Material/Tutorials/md-to-pdf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-TITLE="tutorial"
-FILE="$1"
-# TODO: support latex in markdown
-pandoc -t html5 ${FILE} -o $(basename ${FILE}).pdf -s --pdf-engine=wkhtmltopdf --metadata pagetitle="${TITLE}" --highlight-style="pygments"
