efc3101085b5c44727e8b7cc84160cf98410df70
Yair Twito <yairt16@gmail.com>
2022-03-22 18:24:04 +0200

    hw/1: add files

---
+++ hw/1/w2.py
@@ -0,0 +1,9 @@
+from typing import List, Set
+
+def chars_range(start: str, end: str) -> Set[str]:
+    # hint: `ord` and `chr` may be useful
+    raise NotImplementedError  # delete this line
+
+
+def process_words(s: List[str]) -> List[Set[str]]:
+    raise NotImplementedError  # delete this line
