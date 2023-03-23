6e501b78ef65196466767f8498694ef21b699341
Adi Harif <adi.harif@campus.technion.ac.il>
2023-03-23 13:53:03 +0200

    added hw1 supplied files

---
+++ HW/Homework1/q1.pas
@@ -0,0 +1,19 @@
+program PascalTriangle;
+{
+    Here you may write helper functions and procedures.
+    Please do.
+}
+
+var n: integer;
+var line: array { Complete the array definition... }
+
+begin
+{ Initialize 'line' array }
+ReadLn(n);
+for i := 1 to n do
+    begin
+        {
+            Construct the new line and then print it.
+        }
+    end;
+end.
